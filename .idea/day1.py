69b8a711b8b6a7f7aca13895676e0741ae2bb684
yc2768 <2768647687@qq.com>
2017-10-11 09:06:31 +0800

    day1

--- .idea/day1.py
+++ .idea/day1.py
@@ -5,3 +5,70 @@
 # print "hello world"
 # print ("hello world")
 # print "你好"
+
+#行和缩进
+'''
+if True:
+    print "True"
+else:
+    print "False"
+'''
+
+#多行语句
+"""
+total=item_one+\
+      item_two+\
+      item_three
+"""
+
+# days=['Monday','Tuesday','Wednesday',
+#       'Thrsday','Friday']
+
+# Python 引号
+# word='word'
+# sentence="这是一个句子。"
+# paragraph="""这是一个段落。
+# 包含多个语句"""
+
+# name = "Madisetti" #单行注释
+'''
+这是多行注释，使用单引号。
+这是多行注释，使用单引号。
+'''
+"""
+这是多行注释，使用双引号。
+这是多行注释，使用双引号。
+"""
+
+# 等待用户输入
+# raw_input("\n\n Press the enter key to exit")
+
+# 同一行显示多条语句
+# import sys; x='runoob'; sys.stdout.write(x+'\n')
+
+# Print 输出
+# x="a"
+# y="b"
+#换行输出
+'''print x
+print y'''
+print '----------'
+# 不换行输出
+# print x,
+# print y,
+# 不换行输出
+# print x,y
+
+# 变量赋值
+"""counter=100 #赋值整型变量
+miles=1000.0 # 浮点型
+name="John"  # 字符串
+print counter
+print miles
+print name"""
+
+# Python字符串
+str='Hello World!'
+print str     # 输出完整字符串
+print str[0]  # 输出字符串中的第一个字符
+print str[2:5]
